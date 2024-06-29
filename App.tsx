8754944b74efb482d85ae6152c4a39cc52388abc
NTPbreak <patricknegoue197@gmail.com>
2024-06-29 20:47:09 +0100

    Ajout de react-vision-camera

--- App.tsx
+++ App.tsx
@@ -3,112 +3,105 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Button } from 'react-native';
 import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import Vector from "react-native-vector-icons/MaterialCommunityIcons"
-import { Provider } from 'react-redux';
 import Authentification from "./src/fronted/screen/auth";
-import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
-import { store } from './src/fronted/redux/store.ts';
+import { Provider,useSelector,useDispatch } from 'react-redux'
+import { store } from './src/fronted/redux/store';
 
-const PremierEcran = () => {
+import { decrement,increment } from './src/fronted/redux/slices/authSlicer';
+
+function PremierEcran() {
   const navigation = useNavigation();
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      NTP Screen
-    </View>
-  )
-}
 
-const DetailsScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      Details Screen
-      Notre variable est
+      <Text>NTP Screen</Text>
+
     </View>
-  )
+  );
+}
+function DetailsScreen() {
+
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>Notre variable est </Text>
+    
+
+    </View>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-  const navigation = useNavigation();
-  // ... other code ...
-
-  // Animated styles for tab bar
-  const selectedTab = useSharedValue(0);
-  const animatedStyle = useAnimatedStyle(() => {
-    const translateX = withTiming(selectedTab.value * 60, { duration: 200 });
-    return {
-      transform: [{ translateX }],
-    };
-  });
-
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false, // Hide text labels
-        tabBarStyle: {
-          backgroundColor: '#141414', // Black background
-          height: 70, // Adjust as needed
-          position: 'absolute',
-          bottom: 20,
-          left: 20, // Add padding to avoid overlapping with edges
-          right: 20, // Add padding to avoid overlapping with edges
-          borderRadius: 30, // Rounded corners
-          paddingHorizontal: 20, // Add padding
-          elevation: 0, // Remove shadow
-          shadowOpacity: 0,
-          padding: 20
-        },
-        tabBarActiveTintColor: '#fff', // White active icon color
-        tabBarInactiveTintColor: '#888', // Gray inactive icon color
 
-      }}
+    <Tab.Navigator screenOptions={{ headerShown: false }}
     >
-      {/* ... your tabs  */}
-
-      {/* Add the onTabPress event to update the selectedTab value */}
-      <Tab.Screen
-        name="Home"
-        component={PremierEcran}
+      <Tab.Screen options={
+        {
+          tabBarIcon: ({ color, size }) => (
+            <Vector name="home" color={color} size={size} />
+          ),
+        }
+      } name="Home" component={PremierEcran} />
+      <Tab.Screen name="Découvrir"
         options={
           {
             tabBarIcon: ({ color, size }) => (
-              <Vector name="home-outline" color={color} size={size} />
+              <Vector name="compass" color={color} size={size} />
             ),
 
           }
         }
-      />
-
-      {/* ... your other tabs, update the selectedTab.value accordingly  */}
-      <Tab.Screen
-        name="Découvrir"
-        component={DetailsScreen}
+        component={DetailsScreen} />
+      <Tab.Screen name="Ajout"
         options={
           {
             tabBarIcon: ({ color, size }) => (
-              <Vector name="compass-outline" color={color} size={size} />
+              <Vector name="plus" color={color} size={size} />
             ),
 
           }
         }
-      />
+        component={DetailsScreen} />
+      <Tab.Screen name="Boite de reception"
+        options={
+          {
+            tabBarIcon: ({ color, size }) => (
+              <Vector name="mail" color={color} size={size} />
+            ),
 
-      {/* ... your other tabs, update the selectedTab.value accordingly  */}
+          }
+        }
+        component={DetailsScreen} />
+      <Tab.Screen name="Moi"
+        options={
+          {
+            tabBarIcon: ({ color, size }) => (
+              <Vector name="account" color={color} size={size} />
+            ),
 
-    </Tab.Navigator>
+          }
+        }
+        component={DetailsScreen} />
+
+    </Tab.Navigator >
+
   );
 }
-
-// ... your other code ...
 
 function App() {
   const [login, setLogin] = React.useState(true)
   return (
 
+
     <Provider store={store}>
-      <NavigationContainer   >
+
+      <NavigationContainer >
         <MyTabs />
       </NavigationContainer>
     </Provider>
--- src/fronted/redux/store.ts
+++ src/fronted/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authSlicer from './slices/authSlicer'
 
 export const store = configureStore(
     {
-        reducer:{},
+        reducer:{
+            auth: authSlicer
+        },
     }
 )
 
